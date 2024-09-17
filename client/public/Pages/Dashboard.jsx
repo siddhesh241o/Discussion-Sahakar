161ea5aaa653ff4069fa3df3d5b14bd5ebf2c36c
Prasad Ajay Mahankal <150546889+Prasad-Mahankal27@users.noreply.github.com>
2024-09-17 07:19:37 +0530

    Updated Dashboard.jsx

--- client/public/Pages/Dashboard.jsx
+++ client/public/Pages/Dashboard.jsx
@@ -1,53 +1,71 @@
-import React from 'react'
+import React from 'react';
 import { useNavigate } from 'react-router-dom';
 
 const Dashboard = () => {
-    const Navigate = useNavigate();
-  return <>
-  <h1>DashBoard</h1>
-  <div className="cards-list">
-    <div className="card">
-        <h2>GIS</h2>
-        <button onClick={()=>{Navigate('/GIS')}}>Enter</button>
-    </div>
-    <div className="card">
-        <h2>GEO Tagging</h2>
-        <button onClick={()=>{Navigate('/Geotagging')}}>Enter</button>
-    </div>
-    <div className="card">
-        <h2>Meeting</h2>
-        <button onClick={()=>{Navigate('/Meeting')}}>Enter</button>
-    </div>
-    <div className="card">
-        <h2>My Profile</h2>
-        <button onClick={()=>{Navigate('/MyProfile')}}>Enter</button>
-    </div>
-    <div className="card">
-        <h2>Inventory</h2>
-        <button onClick={()=>{Navigate('/Inventory')}}>Enter</button>
-    </div>
-    <div className="card">
-        <h2>Complaints</h2>
-        <button onClick={()=>{Navigate('/Complaints')}}>Enter</button>
-    </div>
-    <div className="card">
-        <h2>Projects Directory</h2>
-        <button onClick={()=>{Navigate('/Projects')}}>Enter</button>
-    </div>
-    <div className="card">
-        <h2>TaskManager</h2>
-        <button onClick={()=>{Navigate('/Taskmanager')}}>Enter</button>
-    </div>
-    <div className="card">
-        <h2>Templates</h2>
-        <button onClick={()=>{Navigate('/Templates')}}>Enter</button>
-    </div>
-    <div className="card">
-        <h2>Seminars and Workshops</h2>
-        <button onClick={()=>{Navigate('/Seminar')}}>Enter</button>
-    </div>
-  </div>
-  </>
+    const navigate = useNavigate();
+
+    return (
+        <>
+            <div className="cards-list">
+                
+                <div className="card-container" onClick={() => navigate('/GIS')}>
+                    <div className="card" style={{ backgroundImage: 'url(public/Images/gis.png)', backgroundSize: 'cover', backgroundPosition: 'center' }}>
+                    </div>
+                    <div className="card-label">GIS</div>
+                </div>
+                <div className="card-container" onClick={() => navigate('/Geotagging')}>
+                    <div className="card" style={{ backgroundImage: 'url(public/Images/geotag.png)', backgroundSize: 'cover', backgroundPosition: 'center' }}>
+                    </div>
+                    <div className="card-label">Geo Tagging</div>
+                </div>
+                <div className="card-container" onClick={() => navigate('/Projects')}>
+                    <div className="card" style={{ backgroundImage: 'url(public/Images/project.png)', backgroundSize: 'cover', backgroundPosition: 'center' }}>
+                    </div>
+                    <div className="card-label">Projects Directory</div>
+                </div>
+                <div className="card-container" onClick={() => navigate('/Meeting')}>
+                    <div className="card" style={{ backgroundImage: 'url(public/Images/meet.png)', backgroundSize: 'cover', backgroundPosition: 'center' }}>
+                    </div>
+                    <div className="card-label">Meeting Scheduling</div>
+                </div>
+                <div className="card-container" onClick={() => navigate('/Taskmanager')}>
+                    <div className="card" style={{ backgroundImage: 'url(public/Images/task.png)', backgroundSize: 'cover', backgroundPosition: 'center' }}>
+                    </div>
+                    <div className="card-label">Task Manager</div>
+                </div>
+                <div className="card-container" onClick={() => navigate('/Seminar')}>
+                    <div className="card" style={{ backgroundImage: 'url(public/Images/seminar.png)', backgroundSize: 'cover', backgroundPosition: 'center' }}>
+                    </div>
+                    <div className="card-label">Seminars and Workshops</div>
+                </div>
+                <div className="card-container" onClick={() => navigate('/Discussion')}>
+                    <div className="card" style={{ backgroundImage: 'url(public/Images/discussion.png)', backgroundSize: 'cover', backgroundPosition: 'center' }}>
+                    </div>
+                    <div className="card-label">Discussions</div>
+                </div>
+                <div className="card-container" onClick={() => navigate('/Inventory')}>
+                    <div className="card" style={{ backgroundImage: 'url(public/Images/inventory.png)', backgroundSize: 'cover', backgroundPosition: 'center' }}>
+                    </div>
+                    <div className="card-label">Inventory</div>
+                </div>
+                <div className="card-container" onClick={() => navigate('/Templates')}>
+                    <div className="card" style={{ backgroundImage: 'url(public/Images/template.png)', backgroundSize: 'cover', backgroundPosition: 'center' }}>
+                    </div>
+                    <div className="card-label">Templates</div>
+                </div>
+                <div className="card-container" onClick={() => navigate('/Complaints')}>
+                    <div className="card" style={{ backgroundImage: 'url(public/Images/complaints.png)', backgroundSize: 'cover', backgroundPosition: 'center' }}>
+                    </div>
+                    <div className="card-label">Complaints</div>
+                </div>
+                {/* <div className="card-container" onClick={() => navigate('/MyProfile')}>
+                    <div className="card" style={{ backgroundImage: 'url(public/Images/profile.png)', backgroundSize: 'cover', backgroundPosition: 'center' }}>
+                    </div>
+                    <div className="card-label">My Profile</div>
+                </div> */}
+            </div>
+        </>
+    );
 }
 
 export default Dashboard;
