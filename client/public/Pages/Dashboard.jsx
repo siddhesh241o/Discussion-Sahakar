6a901b6a2bb8fadb15fce2dfc13f643e47666d8d
Siddhesh Patil <siddheshpatil4567@gmail.com>
2024-09-21 17:53:53 +0530

    fixed dashboard

--- client/public/Pages/Dashboard.jsx
+++ client/public/Pages/Dashboard.jsx
@@ -9,32 +9,32 @@ const Dashboard = () => {
             <div className="cards-list">
                 
                 <div className="card-container" onClick={() => navigate('/Geotagging')}>
-                    <div className="card" style={{ backgroundImage: 'url(public/Images/gis.png)', backgroundSize: 'cover', backgroundPosition: 'center' }}>
+                    <div className="card" style={{ backgroundImage: 'url(public/Images/gisNew.png)', backgroundSize: 'cover', backgroundPosition: 'center' }}>
                     </div>
                     <div className="card-label">GIS</div>
                 </div>
                  <div className="card-container" onClick={() => navigate('/GIS')}>
-                    <div className="card" style={{ backgroundImage: 'url(public/Images/geotag.png)', backgroundSize: 'cover', backgroundPosition: 'center' }}>
+                    <div className="card" style={{ backgroundImage: 'url(public/Images/geotagging.png)', backgroundSize: 'cover', backgroundPosition: 'center' }}>
                     </div>
                     <div className="card-label">Geo Tagging</div>
                 </div>
                 <div className="card-container" onClick={() => navigate('/Projects')}>
-                    <div className="card" style={{ backgroundImage: 'url(public/Images/project.png)', backgroundSize: 'cover', backgroundPosition: 'center' }}>
+                    <div className="card" style={{ backgroundImage: 'url(public/Images/projectdirectory.png)', backgroundSize: 'cover', backgroundPosition: 'center' }}>
                     </div>
                     <div className="card-label">Projects Directory</div>
                 </div>
                 <div className="card-container" onClick={() => navigate('/Meeting')}>
-                    <div className="card" style={{ backgroundImage: 'url(public/Images/meet.png)', backgroundSize: 'cover', backgroundPosition: 'center' }}>
+                    <div className="card" style={{ backgroundImage: 'url(public/Images/meeting.png)', backgroundSize: 'cover', backgroundPosition: 'center' }}>
                     </div>
                     <div className="card-label">Meeting Scheduling</div>
                 </div>
                 <div className="card-container" onClick={() => navigate('/Taskmanager')}>
-                    <div className="card" style={{ backgroundImage: 'url(public/Images/task.png)', backgroundSize: 'cover', backgroundPosition: 'center' }}>
+                    <div className="card" style={{ backgroundImage: 'url(public/Images/taskManager.png)', backgroundSize: 'cover', backgroundPosition: 'center' }}>
                     </div>
                     <div className="card-label">Task Manager</div>
                 </div>
                 <div className="card-container" onClick={() => navigate('/Seminar')}>
-                    <div className="card" style={{ backgroundImage: 'url(public/Images/seminar.png)', backgroundSize: 'cover', backgroundPosition: 'center' }}>
+                    <div className="card" style={{ backgroundImage: 'url(public/Images/seminarWorkshop.png)', backgroundSize: 'cover', backgroundPosition: 'center' }}>
                     </div>
                     <div className="card-label">Seminars and Workshops</div>
                 </div>
@@ -44,17 +44,17 @@ const Dashboard = () => {
                     <div className="card-label">Discussions</div>
                 </div>
                 <div className="card-container" onClick={() => navigate('/Inventory')}>
-                    <div className="card" style={{ backgroundImage: 'url(public/Images/inventory.png)', backgroundSize: 'cover', backgroundPosition: 'center' }}>
+                    <div className="card" style={{ backgroundImage: 'url(public/Images/inventoryNew.png)', backgroundSize: 'cover', backgroundPosition: 'center' }}>
                     </div>
                     <div className="card-label">Inventory</div>
                 </div>
                 <div className="card-container" onClick={() => navigate('/Templates')}>
-                    <div className="card" style={{ backgroundImage: 'url(public/Images/template.png)', backgroundSize: 'cover', backgroundPosition: 'center' }}>
+                    <div className="card" style={{ backgroundImage: 'url(public/Images/templateNew.png)', backgroundSize: 'cover', backgroundPosition: 'center' }}>
                     </div>
                     <div className="card-label">Templates</div>
                 </div>
                 <div className="card-container" onClick={() => navigate('/Complaints')}>
-                    <div className="card" style={{ backgroundImage: 'url(public/Images/complaints.png)', backgroundSize: 'cover', backgroundPosition: 'center' }}>
+                    <div className="card" style={{ backgroundImage: 'url(public/Images/complainNew.png)', backgroundSize: 'cover', backgroundPosition: 'center' }}>
                     </div>
                     <div className="card-label">Complaints</div>
                 </div>
